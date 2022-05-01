6bb8902f58fa27e2f77969640346063d72f2fb52
Gustavo Silva <gustavo.athenas13@gmail.com>
2022-05-01 14:48:29 -0300

    Inicio do projeto - coletor

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,90 @@
+import {useForm} from 'react-hook-form' 
+import { useEffect } from 'react'
+import Quagga from 'quagga'
+export default function Form() {
+
+    useEffect(() => {
+        if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+            Quagga.init({
+                inputStream: {
+                    name: "Live",
+                    type: "LiveStream",
+                    target: document.querySelector("#etiqueta"),
+                    constraints: {
+                        facingMode: "environment",
+                    },
+                },
+                numOfWorkers: 1,
+                locate: true,
+                decoder: {
+                    readers: ['ean_reader']
+                }
+            },
+            err => {
+                if(err) {
+                    console.log(err)
+                    alert("erro ao abri a camera do dispositivo")
+                    return
+                }
+                Quagga.start()
+            }
+            
+            );
+        }
+    },[]);
+    const {register, handleSubmit, watch, formState: { errors }} = useForm()
+
+    function userDate(data){
+        console.log(data)
+    }
+    console.log(errors)
+    return (
+        <form onSubmit={handleSubmit(userDate)}>
+            <div>
+                <label htmlFor="">
+                    Etiqueta
+                    <input type="text" id="etiqueta" {...register('etiqueta', { required: true })}  />
+                    {errors.etiqueta && <span>Input etiqueta é obrigatorio</span>}
+                </label>
+                <button>Consultar</button>
+                <button>Limpar</button>
+                <button>Sair</button>
+                <div>
+                    <input type="text" disabled  {...register('descrição')}/>
+                </div>
+            </div>
+
+            <div>
+                <label htmlFor="">
+                    Status
+                    <input disabled type="text" {...register('status')} />
+                </label>
+                
+            </div>
+
+            
+                <label htmlFor="">
+                    Localização
+                </label>
+                <input type='text' {...register('Localização')}rows="3"/>
+           
+            <button type='submit'>Conferido</button>
+        </form>
+    )
+}
+/*
+todos:
+label - etiqueta
+input - entrada dos dados
+3 botaõ  - consultar - limpar - sair
+
+label - status
+div - conferido ou nao conferido 
+1 botaõ - conferido
+
+
+label - localização
+textArea - onde esta o imobilizado
+
+
+*/
